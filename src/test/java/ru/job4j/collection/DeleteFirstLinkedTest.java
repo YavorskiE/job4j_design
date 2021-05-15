418d4bc69725722995fad69944f64367ac642c1a
Eugen <YavorskiE@outlook.com>
2021-05-15 13:21:56 -0400

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#439049]

--- src/test/java/ru/job4j/collection/DeleteFirstLinkedTest.java
+++ src/test/java/ru/job4j/collection/DeleteFirstLinkedTest.java
@@ -33,4 +33,16 @@ public class DeleteFirstLinkedTest {
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next(), is(2));
     }
+
+    @Test
+    public void whenAddFirstThenIt() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.addFirst(3);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
 }
